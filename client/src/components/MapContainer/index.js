f20c03090c8fbd84244fd63eee04486254489281
Natosha <nmsuri28@gmail.com>
2019-04-23 12:30:29 -0500

    more on geolocation

--- client/src/components/MapContainer/index.js
+++ client/src/components/MapContainer/index.js
@@ -1,68 +1,178 @@
-// import React from 'react';
-// import {GoogleApiWrapper, Map, Marker} from "google-maps-react";
-// //import  React_App_API_KEY_G from './config_keys'
-// //const API_KEY = React_App_API_KEY_G
-// //console.log(API_KEY);
+import React from 'react';
+import {GoogleApiWrapper, Map, Marker, InfoWindow} from "google-maps-react";
+import axios from 'axios';
+import  React_App_API_KEY_G from './config_keys'
+const API_KEY = React_App_API_KEY_G
 
 
 
-// export class MapContainer extends React.Component {
-//     state = {
-//         userLocation:{
-//             lat: 32, 
-//             lng: 32
-//         },
-//         loading:true
-//         };
-
-
-//     componentDidMount(props){
-//         navigator.geolocation.getCurrentPosition(
-//             position => {
-//                 const {latitude, longitude} = position.coords;
-
-//                 this.setState({
-//                     userLocation:{
-//                         lat: latitude,
-//                         lng: longitude
-//                     },
-//                     loading:false
-//                 });
-
-//             console.log(this.state.userLocation);
-//             },
-//             ()=> {
-//                 this.setState({ loading:false});
-//             }
-//         );
-    
-//     }
-//     render(){
-//         const{loading, userLocation} =this.state;
-//         const {google}= this.props;
-
-//         if(loading){
-//             return null;
-//         }
-
-//         return(
-//             // <div className="mt-5 ml-5">
-//             <Map
-//             google={google}
-//             //can be pushed into css file
-//             style={{ width: "auto", height: "5%"}}
-//             initialCenter={userLocation}
-//             zoom={10}
-//           >
-//             <Marker name={"Your position"} position={this.state.userLocation} />
-//           </Map>
-//         //   </div>
-//         );
-//     }
-//     }
-
-    
-//     export default GoogleApiWrapper({
-//         apiKey: "AIzaSyB51AI9jcTzVBa-THgUie9uT87HiyvtDmE"
+// const latitude=0;
+// const longitude=0;
+export class MapContainer extends React.Component {
+    state = {
         
-//     })(MapContainer);
+            lat: 0, 
+            lng: 0,
+            county: "",
+            zip:0,
+            state:"",
+        loading:true,
+        showingInfoWindow:false,
+        activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {}
+        };
+
+
+    componentDidMount(props){
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const {latitude, longitude} = position.coords;
+                
+                this.setState({
+                    
+                        lat: latitude,
+                        lng: longitude,
+                    
+                    loading:false
+                });
+            const updatedLatitude = this.state.lat;
+            const updatedLongitude =this.state.lng;
+
+            console.log("State: " ,this.state.lat);
+            console.log("State: " , this.state.lng);
+            
+            console.log(updatedLatitude);
+            console.log(updatedLongitude);
+            this.handleChange(updatedLatitude, updatedLongitude);
+            
+            //  console.log(latitude);
+            //  console.log(longitude);
+            },
+            ()=> {
+                this.setState({ loading:false});
+            }
+            
+        );
+        this.handleChange=(updatedLatitude, updatedLongitude) => {
+            const API_URL ='https://us1.locationiq.com/v1/reverse.php?key=69b410ea8cf9cb&lat=' +updatedLatitude+ '&lon=' +updatedLongitude+'&format=json';
+           axios.get(API_URL).then(response => {
+            
+            console.log(API_URL)
+            const data = response;
+            const county=data.data.address.county;
+            const zip = data.data.address.postcode;
+            const state=data.data.address.state;
+            
+                this.setState({
+                    county: county,
+                    zip: zip,
+                    state: state
+                });
+            
+            
+            console.log(data);
+            console.log(this.state.county);
+            console.log(this.state.zip);
+            console.log(this.state.state);
+            },
+           
+            
+                
+          
+            // console.log(this.state.zip)
+           
+        
+        
+           
+           
+           
+              
+            //    console.log(zip);
+           
+       //         function success(response) {
+       //            console.log('User\'s Location Data is ', response);
+       //             console.log('Users zipcode ' , response.address.postcode);
+       //             var zipCode=JSON.parse(response.address.postcode);
+       //             console.log('Yay! Zipcode has been added: ' +zipCode);
+       //             // zipcodeRadius(zipCode);
+       //  })
+          
+       
+           
+       )
+       
+         
+    }
+    
+}
+
+    onMarkerClick =(props, marker, e) =>
+    this.setState({
+        selectedPlace: props,
+      activeMarker: marker,
+        showingInfoWindow:true
+    });
+    onClose = props => {
+        if (this.state.showingInfoWindow) {
+          this.setState({
+            showingInfoWindow: false,
+            activeMarker: null
+          });
+        }
+      };
+     
+
+
+    
+    render(){
+        const{loading ,lat,lng} =this.state;
+        const {google}= this.props;
+
+        if(loading){
+            return null;
+        }
+    
+
+        return(
+            // <div className="mt-5 ml-5">
+            <Map
+            google={google}
+            //can be pushed into css file
+            style={{ width: "auto", height: "5%"}}
+            initialCenter={{
+                lat: this.state.lat,
+                lng: this.state.lng
+            }}
+                
+            zoom={10}
+          >
+            <Marker 
+            onClick={this.onMarkerClick}
+            name={{
+                county: this.state.county,
+                state: this.state.state
+            }} position={
+                {lat: this.state.lat,
+                lng: this.state.lng}} 
+                /> 
+                {/* Does not work */}
+                {/* <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow> */}
+          </Map>
+        //   </div>
+        );
+    };
+    
+}
+    
+    export default GoogleApiWrapper({
+        apiKey: API_KEY
+        
+    })(MapContainer);
