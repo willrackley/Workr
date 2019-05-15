c60700239feb8d1f3e73c5ff786896c83b7997c6
williamrackley <willrackley65@gmail.com>
2019-05-15 14:43:04 -0400

    minor changes

--- client/src/components/MessageModal/index.js
+++ client/src/components/MessageModal/index.js
@@ -10,7 +10,7 @@ export default function MessageModal(props) {
             <div className="modal-dialog" role="document">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h5 className="modal-title d-block" id="exampleModalLabel">To: {newModal.posterName}</h5>
+        <h5 className="modal-title d-block" id="exampleModalLabel">{newModal.username !== null ? (<span className="text-capitalize">To: {newModal.username}</span>) : (<span className="text-capitalize">To: {newModal.posterName}</span>)}</h5>
                         <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                         </button>
